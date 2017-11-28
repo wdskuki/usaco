bbf48f2d1cd7ea24e19dc26d784c1f53a130b431
Dongsheng Wei <weidongsheng@dajiazhongyi.com>
2017-11-28 16:56:02 +0800

    wormhole

--- wormhole.cpp
+++ wormhole.cpp
@@ -2,7 +2,7 @@
 /*
 ID: dswei191
 LANG: C++
-TASK: combo
+TASK: wormhole
 */
 
 #include <iostream>
@@ -11,122 +11,164 @@ TASK: combo
 #include <algorithm>
 #include <cmath>
 using namespace std;
-ofstream fout("main.out");
-ifstream fin("main.in");
+ofstream fout("wormhole.out");
+ifstream fin("wormhole.in");
 
-int n;
+#define MAX_N 12
+
+int N;
 int ret = 0;
-class Position{
-public:
-    int x, y;
-    int pairIndex;
-    bool visited;
-    Position(int x, int y){
-        this->x = x;
-        this->y = y;
-        this->pairIndex = -1;
-        this->visited = false;
-    }
-    void clear(){
-        this->pairIndex = -1;
-        this->visited = false;
-    }
-};
-vector<Position> vec;
-
-void reset(){
-    for(int i = 0; i < n; i++){
-        vec[i].clear();
+vector<int> pairs(MAX_N+1, -1); //pairs[i] = j,表示i和j是一对
+vector<int> nxt(MAX_N+1, -1); //next[i] = j, 表示i的右边第一个为j
+vector<pair<int, int> > wormholes(MAX_N+1); //表示wormhole的坐标
+vector<bool> visit(MAX_N+1, false); //visit[i],表示i是否被访问过
+void print(vector<int> vec){
+    for(int i = 1; i <=N; i++){
+        cout<< vec[i] <<endl;
     }
 }
+//检测是否有环路
 
-int hasNext(int index){
-    bool flag = false;
-    for(int i = 0; i < n; i++){
-        if(i != index && vec[i].y == vec[index].y && vec[i].x > vec[index].x){
-            flag = true;
-            if(vec[i].visited == true){
-                return -1;
+bool checkLoop(int i) {
+    /**
+     * 从一个点出来，一直找到他右侧的第一个点
+     *
+     * 1. 如果没有找到，则失败
+     * 2. 如果找到了 i，且已经被访问过，则成功
+     * 3. 如果找到了 i，且没有被访问过，则进入到改点的配对点 j，继续checkLoop
+     */
+
+//    if(nxt[i] == -1){
+//        return false;
+//    }
+//
+//    //存在右侧节点
+//    if(visit[nxt[i]] == true){
+//        return true;
+//    }
+//    //右侧节点没有被访问过
+//    visit[nxt[i]] = true;
+//
+//    return checkLoop(pairs[nxt[i]]);
+
+
+    if(visit[i] == true){
+        return true;
+    }
+
+    visit[i] = true;
+
+    if(nxt[pairs[i]] == -1){
+        return false;
+    }else{
+        return checkLoop(nxt[pairs[i]]);
+    }
+
+}
+
+bool checkLoop(){
+    for(int i = 1; i <= N; i++){
+        bool noLoop = false;
+        int start = i;
+        for(int j = 0; j < N; j++){
+            int pairIndex = pairs[start];
+            if(nxt[pairIndex] == -1){
+                noLoop = true;
+                break;
             }else{
-                return i;
+                start = nxt[pairIndex];
+            }
+        }
+        if(noLoop == false){
+            return true;
+        }
+    }
+    return false;
+}
+
+void makePairs(){
+    int i;
+    for(i = 1; i <= N; i++){
+        if(pairs[i] == -1)
+            break;
+    }
+    if( i > N){
+        //所有虫洞都配对好了
+//        if(checkLoop()){
+//            ret += 1;
+//        }
+        for(int k = 1; k <= N; k++){
+            for(int t = 1; t <= N; t++){
+                visit[t] = false;
+            }
+            if(checkLoop(k)){
+                ret +=1;
+                break;
             }
         }
     }
-    if(flag == false){
-        return -2;
-    }
-}
-
-bool checkLoop(int index){
-    if(vec[index].visited = true){
-        return true;
-    }else{
-        vec[index].visited = true;
-
-        /**
-         * 找到右边的节点
-         * 如果右边没有节点，则返回失败
-         * 如果右边有节点，且该节点没有被访问过，则继续checkLoop
-         * 如果右边有节点，且该节点被访问过，则返回true
-         */
-        int rightIndex = hasNext(index);
-        if(rightIndex == -2){
-            return false; //右侧没有节点
-        }else if(rightIndex == -1){
-            return true;
-        }else{
-            return checkLoop(rightIndex);
+    for(int j = i+1; j <= N; j++){
+        if(pairs[j] == -1){
+            pairs[i] = j;
+            pairs[j] = i;
+            makePairs();
+            pairs[i] = -1;
+            pairs[j] = -1;
         }
     }
 }
-void genPairs(int index){
-    if(index == n - 1){
-        for(int i = 0; i < n; i++){
-            if(checkLoop(i)){
+
+void makePairs(int index){
+    if(index > N){
+        //所有的虫洞都配对好了
+        for(int k = 1; k <= N; k++){
+            for(int t = 1; t <= N; t++){
+                visit[t] = false;
+            }
+            if(checkLoop(k)){
                 ret +=1;
-                reset();
                 break;
             }
         }
         return;
     }
-    if(vec[index].pairIndex != -1){
-        genPairs(index + 1);
+    if(pairs[index] != -1){
+        makePairs(index + 1);
+        return;
     }else{
-        for(int i = index + 1; i < n; i++){
-            if(vec[i].pairIndex == -1){
-                vec[index].pairIndex = i;
-                vec[i].pairIndex = index;
-                genPairs(index+1);
-                vec[index].pairIndex = -1;
-                vec[i].pairIndex = -1;
+        for(int i = index + 1; i <= N; i++){
+            if(pairs[i] == -1){
+                pairs[index] = i;
+                pairs[i] = index;
+                makePairs(index + 1);
+                pairs[i] = pairs[index] = -1;
             }
         }
     }
 }
 
-bool comp(Position a, Position b){
-    return a.x < b.x;
-}
 int main(){
-    ret = 0;
-    fin >> n;
-    for(int i = 0; i < n; i++){
+    fin>>N;
+    for(int i = 1; i <=N; i++){
         int x, y;
-        fin >> x >> y;
-        Position p(x, y);
-        vec.push_back(p);
+        fin>>x>>y;
+        wormholes[i] = make_pair(x, y);
     }
 
-    sort(vec.begin(), vec.end(), comp);
+    for(int i = 1; i <= N; i++){
+        int nextIndex = -1;
+        for(int j = 1; j <= N; j++){
+            if(wormholes[i].second == wormholes[j].second && wormholes[i].first < wormholes[j].first){
+                if(nextIndex < 0 || wormholes[nextIndex].first > wormholes[j].first){
+                    nextIndex = j;
+                }
+            }
+        }
+        nxt[i] = nextIndex;
+    }
 
-//    for(int i = 0; i < n; i++){
-//        cout<<"("<<vec[i].x<<","<<vec[i].y<<")"<<endl;
-//    }
-    genPairs(0);
-    cout<< ret <<endl;
-
-//    cout<<"Hello world"<<endl;
+    makePairs();
+    fout<<ret<<endl;
 
     return 0;
 }
